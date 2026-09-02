b881df022efaf853610f790d992121a5330adad0
Aravindamuthan-Git <aravindamuthan@thestackly.com>
2026-09-02 09:42:36 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,7 +1,35 @@
-from app import add, subtract
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/api/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "UP"
+
 
 def test_add():
-    assert add(10, 20) == 30
+    client = app.test_client()
 
-def test_subtract():
-    assert subtract(20, 10) == 10
+    response = client.get("/api/add?a=10&b=20")
+
+    assert response.status_code == 200
+    assert response.json["result"] == 30
+
+
+def test_add_missing_parameters():
+    client = app.test_client()
+
+    response = client.get("/api/add")
+
+    assert response.status_code == 400
